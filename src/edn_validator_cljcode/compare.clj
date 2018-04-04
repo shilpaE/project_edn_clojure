3ae272f9117deada74bb931752d1d8e2d14ccfe5
ShilpaE <shilpaellur95@gmail.com>
2018-04-04 01:34:30 -0500

    Updated project compares the project id of edn and json

--- src/edn_validator_cljcode/compare.clj
+++ src/edn_validator_cljcode/compare.clj
@@ -16,6 +16,10 @@
 (defn codding [jsonfile ednfile]
   
   ;;Fetching the json file data and editing the names of measures and storing as json-rec
+ (if (and (and (and (.exists (io/file jsonfile)) (.isAbsolute (io/file jsonfile))) (.contains jsonfile ".json"))
+          (and (and (.exists (io/file ednfile)) (.isAbsolute (io/file ednfile))) (.contains ednfile ".edn")))
+(do
+  (try
   (def all-jrec (json/read-str(slurp jsonfile)))
   (def json-rec (into #{}
         (for [ x (get all-jrec "measures")] 
@@ -26,6 +30,10 @@
   (def w (clojure.core/read-string (str " " all-erec " ")))
   (def edn-rec(into #{} (for [y (get w :measures)] 
                        (clojure.string/replace (get y :name) #"/" "."))))
+  (catch java.io.FileNotFoundException e (println (str "caught file
+         exception: " (.getMessage e))))
+      
+      (catch Exception e (println (str "caught exception: " (.getMessage e)))))
   
   ;;comparing the edn and json obtained names 
   ;;The unique files of edn 
@@ -33,22 +41,33 @@
   ;;The unique files of json
   (def jsondata ( clojure.set/difference json-rec edn-rec))
   
+ 
   ;;Checking  files which contain the data and then it will be written to a text file 
-  (try (cond 
-         (and (empty? edndata) (empty? jsondata))(spit "C:\\Users\\se056529\\Documents\\ednvalidator-result.txt" "No Data To Display" :append true)
+ (try (cond 
+         (and (empty? edndata) (empty? jsondata))(spit "C:\\Users\\se056529\\Documents\\ednvalidator-result.txt" "\n There are no unique files to display \n " :append true)
          (empty? edndata) ((spit "C:\\Users\\se056529\\Documents\\ednvalidator-result.txt" "\n The json data is \n" :append true)
                            (spit "C:\\Users\\se056529\\Documents\\ednvalidator-result.txt" jsondata :append true))
          (empty? jsondata) ((spit "C:\\Users\\se056529\\Documents\\ednvalidator-result.txt" "\n The edn data is \n" :append true)
                             (spit "C:\\Users\\se056529\\Documents\\ednvalidator-result.txt" edndata :append true))
-          :else ( (spit "C:\\Users\\se056529\\Documents\\ednvalidator-result.txt" "\n The unique data is \n" :append true)
-                    (spit "C:\\Users\\se056529\\Documents\\ednvalidator-result.txt" edndata :append true)
+          :else ( (spit "C:\\Users\\se056529\\Documents\\ednvalidator-result.txt" "\n The edn data is \n" :append true)
+                   (spit "C:\\Users\\se056529\\Documents\\ednvalidator-result.txt" edndata :append true)
+                   (spit "C:\\Users\\se056529\\Documents\\ednvalidator-result.txt" "\n The json data is \n" :append true)
                     (spit "C:\\Users\\se056529\\Documents\\ednvalidator-result.txt" jsondata :append true))
           )
      (catch Exception e (println (str " " (.getMessage e)))))
 
- ;;Returning a string 
- (str "done" "") 
+  
+ ;;Comparing the edn and json files program id
+ (def jprgmid(for [ x (get all-jrec "programId")] (clojure.string/replace x #"/" ".")))
+  (def eprgmid (for [y (get w :name)] 
+                     (clojure.string/replace y #"/" ".")))
+  (def a(apply str eprgmid))
+  (def b (apply str jprgmid))
+  (str (= a b))
  )
+(do
+ 
+(str "redo"))))
 
 
 (defn -codding [x y]
@@ -56,7 +75,7 @@
   )
 
 ;;To run on REPL remove the below commented semicolons
-(-codding "C:\\Users\\se056529\\Documents\\choc1.json" "C:\\Users\\se056529\\Downloads\\app-rules-5.0.0\\algorithms\\registry-programs\\cernerstandard.adolescentwellness.clinical.program_ruleset_description.edn")
+;;(-codding "C:\\Users\\se056529\\Documents\\choc1.txt" "C:\\Users\\se056529\\Downloads\\app-rules-5.0.0\\algorithms\\registry-programs\\cernerstandard.adolescentwellness.clinical.program_ruleset_description.edn")
 
 
 (defn -main []
