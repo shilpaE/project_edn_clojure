ee2bb2e917ccc9031f796ceea8319c3be2512b6f
shilpaE <37647635+shilpaE@users.noreply.github.com>
2018-04-05 10:09:21 +0530

    Add files via upload

---
+++ test/edn_validator_cljcode/testing.clj
@@ -0,0 +1,16 @@
+(ns edn_validator_cljcode.testing
+   (:require [clojure.java.io :as io])
+  (:require [clojure.set :as set])
+  (:require [edn_validator_cljcode.compare :refer :all]))
+ (use 'clojure.test)
+ 
+ 
+(deftest codding-test
+ 
+  (is (= "redo" (codding "aaaaa.tfyg" "hgdtfyguhik.yg")))
+  (is (= "false" (codding "C:\\Users\\se056529\\Documents\\choc1.json" "C:\\Users\\se056529\\Downloads\\app-rules-5.0.0\\algorithms\\registry-programs\\cernerstandard.adolescentwellness.clinical.program_ruleset_description.edn")))
+  (is (not= "false" (codding "aaaaa.tfyg" "hgdtfyguhik.yg")))
+  (is (= "redo" (codding "1" "2")))
+ )
+
+(run-tests 'edn_validator_cljcode.testing)
